b8dcc21e93489cbb95b922c5cad45f261ab83fd0
YEJI-324 <kej980324@gmail.com>
2021-05-20 15:33:02 +0900

    Merge branch 'master' of /Users/yejikim/AndroidStudioProjects/SunflowerClone with conflicts.

--- app/src/main/java/com/hello/sunflowerclone/HomeViewPagerFragment.kt
+++ app/src/main/java/com/hello/sunflowerclone/HomeViewPagerFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 class HomeViewPagerFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +16,20 @@ class HomeViewPagerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_view_pager, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val options = navOptions {
+//            anim {
+//                enter = R.anim.slide_in_right
+//                e
+//            }
+//        }
+
+        view.findViewById<Button>(R.id.btn_view_pager)?.setOnClickListener {
+            findNavController().navigate(R.id.action_view_pager_fragment_to_plant_detail_fragment)
+        }
     }
 }
--- app/src/main/java/com/hello/sunflowerclone/PlantDetailFragment.kt
+++ app/src/main/java/com/hello/sunflowerclone/PlantDetailFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class PlantDetailFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +15,13 @@ class PlantDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_plant_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_detail)?.setOnClickListener {
+            findNavController().navigate(R.id.action_plant_detail_fragment_to_gallery_fragment)
+        }
     }
 }
