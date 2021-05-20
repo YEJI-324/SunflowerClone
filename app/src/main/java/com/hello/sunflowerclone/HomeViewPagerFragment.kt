d755a222759914ca40c132c1845051fe06c52d91
YEJI-324 <kej980324@gmail.com>
2021-05-20 15:33:02 +0900

    Merge branch 'master' of /Users/yejikim/AndroidStudioProjects/SunflowerClone with conflicts.

--- app/src/main/java/com/hello/sunflowerclone/HomeViewPagerFragment.kt
+++ app/src/main/java/com/hello/sunflowerclone/HomeViewPagerFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 class HomeViewPagerFragment : Fragment() {
     override fun onCreateView(
@@ -19,6 +20,13 @@ class HomeViewPagerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+//        val options = navOptions {
+//            anim {
+//                enter = R.anim.slide_in_right
+//                e
+//            }
+//        }
 
         view.findViewById<Button>(R.id.btn_view_pager)?.setOnClickListener {
             findNavController().navigate(R.id.action_view_pager_fragment_to_plant_detail_fragment)
