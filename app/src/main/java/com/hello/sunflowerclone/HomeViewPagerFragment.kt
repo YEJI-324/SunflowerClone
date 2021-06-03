21c8db046d4c9a632e42130b152facae7708da83
YEJI-324 <kej980324@gmail.com>
2021-06-03 17:51:33 +0900

    fix(ViewPager) : ViewPager -> ViewPager2 수정

--- app/src/main/java/com/hello/sunflowerclone/HomeViewPagerFragment.kt
+++ app/src/main/java/com/hello/sunflowerclone/HomeViewPagerFragment.kt
@@ -5,12 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.hello.sunflowerclone.adapter.SunflowerPagerAdapter
 
 class HomeViewPagerFragment : Fragment() {
 
-    private lateinit var viewPager : ViewPager
+    private lateinit var viewPager : ViewPager2
     private lateinit var pagerAdapter: SunflowerPagerAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -26,7 +26,7 @@ class HomeViewPagerFragment : Fragment() {
     }
 
     private fun initPager() {
-        pagerAdapter = SunflowerPagerAdapter(requireFragmentManager())
+        pagerAdapter = SunflowerPagerAdapter(this)
         pagerAdapter.addFragment(GardenFragment())
         pagerAdapter.addFragment(PlantListFragment())
     }
--- app/src/main/java/com/hello/sunflowerclone/adapter/SunflowerPagerAdapter.kt
+++ app/src/main/java/com/hello/sunflowerclone/adapter/SunflowerPagerAdapter.kt
@@ -1,23 +1,23 @@
 package com.hello.sunflowerclone.adapter
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
 
-class SunflowerPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+class SunflowerPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
     private val fragments: ArrayList<Fragment> = ArrayList()
 
     fun addFragment(fragment: Fragment) {
         fragments.add(fragment)
+        notifyItemInserted(fragments.size-1)
     }
 
-    override fun getItem(position: Int): Fragment {
-        return fragments[position]
-    }
-
-    override fun getCount(): Int {
+    override fun getItemCount(): Int {
         return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
     }
 }
