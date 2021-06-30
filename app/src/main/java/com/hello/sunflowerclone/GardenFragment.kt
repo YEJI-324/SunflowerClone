8307b5d830f12b53c5dd29847a912d138f8798b2
YEJI-324 <kej980324@gmail.com>
2021-06-30 11:56:53 +0900

    WIP

--- app/src/main/java/com/hello/sunflowerclone/GardenFragment.kt
+++ app/src/main/java/com/hello/sunflowerclone/GardenFragment.kt
@@ -1,10 +1,15 @@
 package com.hello.sunflowerclone
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +25,7 @@ class GardenFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var tv_garden: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +41,32 @@ class GardenFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_garden, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tv_garden = view.findViewById(R.id.tv_garden)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        tv_garden.setOnClickListener {
+            Intent(Intent.ACTION_VIEW).apply {
+//                data = Uri.parse("naversearchapp://default?version=5")
+                data = Uri.parse("http://m.kristinkorea.com/product/project.html?cate_no=116#enp_mbris#enp_bounce&mmkpl=cashw1")
+//                Intent;scheme=naversearchapp;action=android.intent.action.VIEW;category=android.intent.category.BROWSABLE;package=com.nhn.android.search;end
+                //                naversearchapp://명령어?파라미터=옵션&version=버전
+                //                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                //                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//                addCategory(Intent.CATEGORY_BROWSABLE)
+//                setPackage("com.nhn.android.search")
+                Toast.makeText(context, "되는거니,,,,,,", Toast.LENGTH_SHORT).show()
+                Log.d("donquiz", "intent : $this")
+                requireContext().startActivity(this)
+            }
+        }
     }
 
     companion object {
